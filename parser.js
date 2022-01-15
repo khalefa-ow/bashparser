e1567a4075b339f0fba2a58bdedcc5cdb2a39fae
MK <m.elbasiouny@gmail.com>
2022-01-14 19:31:16 -0500

    json formater

--- parser.js
+++ parser.js
@@ -4,12 +4,14 @@ const fs = require('fs')
 
 
 const args = process.argv
-console.log(args[2])
+
 fs.readFile(args[2], 'utf8', (err, data) => {
   if (err) {
     console.error(err)
     return
   }
   const ast = parse(data);
-  console.log(util.inspect(ast, {depth: null}));
+  // console.log(util.inspect(ast, {depth: null}));
+  let output = JSON.stringify(ast, null, 2);
+  fs.writeFileSync(args[2] + '-output.json', output);
 })
