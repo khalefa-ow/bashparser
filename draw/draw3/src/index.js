9e8ef5fc49874d4e6a21d98510b8991ac012e8bf
MK <m.elbasiouny@gmail.com>
2022-01-16 00:28:42 -0500

    Add draw

---
+++ draw/draw3/src/index.js
@@ -0,0 +1,15 @@
+
+import './index.css';
+
+/*dasda*/
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import FlowA from './flow.js'
+
+
+ReactDOM.render(
+    // <React.StrictMode>
+    <FlowA />,
+    // </React.StrictMode>,
+    document.getElementById('root'));
